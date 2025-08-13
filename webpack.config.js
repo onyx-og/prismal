9ea5afcfbf3988521a2beb65163c7d5c7d15d10d
Onyx <hello@onyx.ac>
2025-08-13 11:16:51 +0200

    + header stylesheet

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,7 @@ const components = {
         select: ['./src/components/Form/Select/index.scss', './src/styles/theme.scss'],
         textInput: ['./src/components/Form/TextInput/index.scss', './src/styles/theme.scss'],
         toggle: ['./src/components/Form/Toggle/index.scss', './src/styles/theme.scss'],
+        header: ['./src/components/Header/index.scss', './src/styles/theme.scss'],
         actionBar: ['./src/components/ActionBar/index.scss', './src/styles/theme.scss'],
         stack: ['./src/components/Stack/index.scss', './src/styles/theme.scss'],
         form: ['./src/components/Form/index.scss', './src/styles/theme.scss'],
