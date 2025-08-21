4a581e8eb7cc16e5cbca8e4180b6c771323cf882
Onyx <hello@onyx.ac>
2025-08-21 16:16:32 +0200

    (minor) better type def

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,15 +1,32 @@
 import Dropdown from "components/Dropdown";
 import "./index.scss";
+import ComponentProps from "components/Component";
+import { setBorderRadius } from 'utils/';
 
-export interface TooltipProps {
+export interface TooltipProps extends ComponentProps {
     children?: React.ReactNode; // content
     text: string;
+    accent?: never;
+    accentDark?: never;
+    accentLight?: never;
 }
 
 const Tooltip = (props: TooltipProps) => {
-    const { children,text } = props;
+    const {
+        children,text,
+        className, style,
+        borderRadius
+    } = props;
 
-    return <Dropdown className={"prismal-tooltip"} toggleElement={children}>
+    let style_: {[key: string]: any} = {};
+    setBorderRadius(style_, borderRadius);
+
+    if (style) style_ = {...style_, style};
+
+    let className_ = 'prismal-tooltip';
+    if (className) className_ = `${className_} ${className}`;
+
+    return <Dropdown style={style_} className={className_} toggleElement={children}>
         {text}
     </Dropdown>
 }
