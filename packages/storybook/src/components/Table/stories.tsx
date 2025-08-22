0cf61050a0d36ce996c80f0cd1b83d9c43dbf072
Onyx <hello@onyx.ac>
2025-08-22 17:11:35 +0200

    (draft) Table component

---
+++ packages/storybook/src/components/Table/stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Table from 'components/Table';
+
+export default {
+    title: 'Commons/Table',
+    component: Table,
+    argTypes: {
+        accent: { control: 'color' },
+        accentDark: { control: 'color' },
+        accentLight: { control: 'color' },
+        borderRadius: {
+            control: 'select',
+            options: ["none", "extra-small", "small", "medium", "large", "extra-large", "full"]
+        }
+    }
+} as ComponentMeta<typeof Table>;
+
+const Template: ComponentStory<typeof Table> = (args) => <Table {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    data: {
+        "Q1 2023": {
+            "Product A Sales": 15000,
+            "Product B Sales": 8000,
+            "New Customers": 120,
+            "Region Focus": "North"
+        },
+        "Q2 2023": {
+            "Product A Sales": 18500,
+            "Product B Sales": 9200,
+            "New Customers": 155,
+            "Region Focus": "East"
+        },
+        "Q3 2023": {
+            "Product A Sales": 17000,
+            "Product B Sales": 8500,
+            "New Customers": 130,
+            "Region Focus": "South"
+        },
+        "Q4 2023": {
+            "Product A Sales": 22000,
+            "Product B Sales": 11000,
+            "New Customers": 210,
+            "Region Focus": "West"
+        }
+    }
+};
